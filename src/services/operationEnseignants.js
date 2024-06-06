7a5977f193e774b2c69f3a3db4aa3ba7e4a94c6d
Timothei Corneille <Timothei.Corneille@gmail.com>
2024-06-06 16:31:06 +0200

    Update: Nom des routes axios (Etudiants et enseignants)

--- src/services/operationEnseignants.js
+++ src/services/operationEnseignants.js
@@ -1,29 +1,34 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getAll = (callback) => {
-    axios.get("http://localhost:4000/enseignants").then((res) => callback(res));
-}
+  axios.get("http://localhost:4000/enseignants").then((res) => callback(res));
+};
 
 export const add = (enseignant, callback) => {
-    axios.post('http://localhost:4000/add', enseignant)
-        .then((res) => callback(res))
-        .catch((err) => callback(err));
-}
+  axios
+    .post("http://localhost:4000/enseignants/add", enseignant)
+    .then((res) => callback(res))
+    .catch((err) => callback(err));
+};
 
 export const remove = (id, callback) => {
-    axios.delete(`http://localhost:4000/delete/${id}`).then((res) => callback(res))
-        .catch((err) => callback(err));
-}
+  axios
+    .delete(`http://localhost:4000/enseignants/delete/${id}`)
+    .then((res) => callback(res))
+    .catch((err) => callback(err));
+};
 
 export const update = (id, enseignant, callback) => {
-    axios.put(`http://localhost:4000/update/${id}`, enseignant).then((res) => callback(res))
-        .catch((err) => callback(err));
-}
+  axios
+    .put(`http://localhost:4000/enseignants/update/${id}`, enseignant)
+    .then((res) => callback(res))
+    .catch((err) => callback(err));
+};
 
 export const getEnseignants = () => {
-    getAll((res) => {
-        this.setState({
-            Enseignants: res.data,
-        });
+  getAll((res) => {
+    this.setState({
+      Enseignants: res.data,
     });
+  });
 };
--- src/services/operationEtuds.js
+++ src/services/operationEtuds.js
@@ -1,24 +1,29 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getAll = (callback) => {
   axios.get("http://localhost:4000/etudiants").then((res) => callback(res));
 };
 
 export const add = (etud, callback) => {
-  axios.post('http://localhost:4000/add', etud)
+  axios
+    .post("http://localhost:4000/etudiants/add", etud)
     .then((res) => callback(res))
     .catch((err) => callback(err));
-}
+};
 
 export const remove = (id, callback) => {
-  axios.delete(`http://localhost:4000/delete/${id}`).then((res) => callback(res))
+  axios
+    .delete(`http://localhost:4000/etudiants/delete/${id}`)
+    .then((res) => callback(res))
     .catch((err) => callback(err));
-}
+};
 
 export const update = (id, etud, callback) => {
-  axios.put(`http://localhost:4000/update/${id}`, etud).then((res) => callback(res))
+  axios
+    .put(`http://localhost:4000/etudiants/update/${id}`, etud)
+    .then((res) => callback(res))
     .catch((err) => callback(err));
-}
+};
 
 export const getEtudiants = () => {
   getAll((res) => {
